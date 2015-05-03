806db4a863e0fd6e6e9378aadc330eb8c35daa50
Michele Franzese <mfranzese24@me.com>
2015-05-03 10:14:13 -0400

    Markers appear using View Model!

--- js/script.js
+++ js/script.js
@@ -1,41 +1,81 @@
-//make Google Map appear on Page Load
+//model - list of locations in the Port Jefferson, NY Area
 
-var map;
-function initialize() {
-
-        var mapOptions = {
-          center: { lat: 40.94, lng: -73.06},
-          zoom: 13,
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-
-        setMarkers(map, places);
-};
-
-var places = [
-  	['My House', 40.925594, -73.033634],
-  	['My Boyfriends House', 40.940590, -73.040945],
-  	['Cedar Beach', 40.964386, -73.034274],
-  	['Roccos Pizza',  40.931291, -73.031276],
-  	['Port Jefferson & Bridgeport Ferry', 40.947791, -73.070830],
-  	['Stony Brook University', 40.912364, -73.123378],
-  	['Clinton Avenue Elementary', 40.905240, -73.040907],
-  	['Heritage Park', 40.935747, -73.011568]
+var model = [
+  {
+    "name": "My House",
+    "lat": 40.925594,
+    "lng": -73.033634
+  },
+  {
+    "name": "My Boyfriend's House",
+    "lat": 40.940590, 
+    "lng": -73.040945
+  },
+  {
+    "name": "Cedar Beach",
+    "lat": 40.964386, 
+    "lng": -73.034274
+  },
+  {
+    "name": "Rocco's Pizza",
+    "lat": 40.931291, 
+    "lng": -73.031276
+  },
+  {
+    "name": "Port Jefferson & Bridgeport Ferry",
+    "lat": 40.947791,
+    "lng": -73.070830
+  },
+  {
+    "name": "Stony Brook University",
+    "lat": 40.912364,
+    "lng": -73.123378
+  },
+  {
+    "name": "Clinton Avenue Elementary",
+    "lat": 40.905240,
+    "lng": -73.040907
+  },
+  {
+    "name": "Heritage Park",
+    "lat": 40.935747,
+    "lng": -73.011568
+  }
 ];
 
-function setMarkers(map, locations) {
-	for (var i = 0; i < places.length; i++) {
-		var place = places[i];
-		var myLatLng = new google.maps.LatLng(place[1], place[2]);
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-			title: place[0]
-		});
-	}
-};
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//ViewModel: representation of UI Data & Behaviors
 
+function AppViewModel() {
+
+  var self = this;
+
+  
+  self.placeList = ko.observableArray(model);
+
+  self.map = new google.maps.Map(document.getElementById('map-canvas'),
+      {
+        center: {lat: 40.94, lng: -73.06},
+        zoom: 13
+      }
+      );
+
+  var markers = [];
+  self.initMarkers = function(data) {
+
+    for ( var i = 0; i < data.length; i++ ) {
+
+      var latLng = new google.maps.LatLng( data[i].lat, data[i].lng );
+      var marker = new google.maps.Marker({
+          position: latLng,
+          map: self.map,
+          title: data[i].name
+      });     
+    }
+  }
+  self.initMarkers(this.placeList());
+}
+
+
+ko.applyBindings(new AppViewModel());
 
